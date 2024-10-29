db88793f9b560213e1cd3894db1e05a463500c41
Oscar Espitia <odespitia@sena.edu.co>
2024-10-29 18:36:51 -0500

    ajustes empaquetado

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -15,7 +15,7 @@
 
     figure.mb-5(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/SR3qDlqgNIA" title="El canto y la rítmica musical" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     
 </template>
